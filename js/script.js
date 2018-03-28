cd5a819c73ac05304dcdc7f2f9b8ea396e653d89
Michal Mottl <michal.mottl@coleurope.eu>
2018-03-28 15:50:07 +0700

    operators change class when pressed, prevent adding numbers to result and starting statement with operator

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
 $(document).ready(function() {
     
-var calculation = [];
 var statement = "";
 var expr = "";
 var oper = 0;
+var result = 0;
 
 
     
@@ -26,12 +26,18 @@ var oper = 0;
             }
             
         } else if(val == "%"){
-            expr = expr/100;
+            expr = expr*1/100;
             statement += "/100";
         }
         else {
-            statement += val;
-            expr += val;
+            //////
+            if (result && statement == "" && expr == ""){
+                result = 0;
+            }
+                statement += val;
+                expr += val;
+                console.log(statement);
+            
         }
         if (expr.length > 9){
             $(".screen").css("font-size", "2rem");
@@ -43,40 +49,76 @@ var oper = 0;
     });
 
     $(".oper").click(function(){
-
-
         
-        if (oper == 0) {        
-            var val = $(this).html();
-            statement += val;
-            oper += 1;
-            console.log(statement);
-            $(".screen").html(expr);
+        if ($(".screen").html() == 0){
+            statement = "";
             expr = "";
         } else {
-            var last = statement.substr(statement.length-1);
-            
-            if (["+", "-", "/", "*"].indexOf(last) > -1) {
-                statement = statement.substring(0, statement.length - 1);
-                expr = math.eval(statement).toString();
-                $(".screen").html(expr);
-                var val = $(this).html();
-                statement = expr + val;
-                console.log(statement);
-                expr = "";
-                oper = 1;
+
+            $(".button").css("border", "1px solid black");
+            $(this).css("border", "3px solid white");
+
+                if (result){
+                    expr = result.toString();
+                    console.log(statement);
+                }
 
 
-            } else {
-                expr = math.eval(statement).toString();
-                $(".screen").html(expr);
-                var val = $(this).html();
-                statement = expr + val;
-                expr = "";
-                oper = 1;
+                if (oper == 0) {        
+                    var val = $(this).html();
+                    statement = expr + val;
+                    oper += 1;
+                    console.log(statement);
+                
+                    if (expr.toString().length > 9) {
+                        $(".screen").css("font-size", "2rem");
+                    } else {
+                        $(".screen").css("font-size", "4rem");
+                    }
+                
+                    $(".screen").html(expr);
+                    expr = "";
+                } else {
+                    var last = statement.substr(statement.length-1);
+
+                    if (["+", "-", "/", "*"].indexOf(last) > -1) {
+                        var substat = statement.substring(0, statement.length - 1);
+                        //statement = statement.substring(0, statement.length - 1);
+                        statement = math.eval(substat).toString();
+                        //expr = math.eval(statement).toString();
+
+                        if (statement.length > 9) {
+                            $(".screen").css("font-size", "2rem");
+                        } else {
+                            $(".screen").css("font-size", "4rem");
+                        }
+
+                        $(".screen").html(statement);
+                        var val = $(this).html();
+                        statement += val;
+                        console.log(statement);
+                        expr = "";
+                        oper = 1;
+                    
+                    
+                    } else {
+                        expr = math.eval(statement).toString();
+                    
+                        if (expr.toString().length > 9) {
+                            $(".screen").css("font-size", "2rem");
+                        } else {
+                            $(".screen").css("font-size", "4rem");
+                        }
+                    
+                        $(".screen").html(expr);
+                        var val = $(this).html();
+                        statement = expr + val;
+                        expr = "";
+                        oper = 1;
+                    }
+
+                }
             }
-            
-        }
         
     });
 
@@ -84,6 +126,7 @@ var oper = 0;
         oper = 0;
         expr = "";
         statement = "";
+        result = "";
         $(".screen").css("font-size", "4rem");
         console.log("0");
         $(".screen").html("0");
@@ -91,11 +134,24 @@ var oper = 0;
 
 
     $(".button--eq").click(function(){
+        $(".button").css("border", "1px solid black");
+        var last = statement.substr(statement.length-1);
+            
+            if (["+", "-", "/", "*"].indexOf(last) > -1) {
+                var substat = statement.substring(0, statement.length - 1);
+                //statement = statement.substring(0, statement.length - 1);
+                statement = math.eval(substat).toString();
+                //expr = math.eval(statement).toString();
+            }
+        
         oper = 0;
-        var result = math.eval(statement);
-        statement = result.toString();
-        expr = statement;
+        result = math.eval(statement);
+        //statement = result.toString();
+        //expr = statement;
+        expr = "";
+        statement = "";
         console.log(result);
+        console.log(statement);
         $(".screen").html(result);
         if (result.toString().length > 9) {
             $(".screen").css("font-size", "2rem");
